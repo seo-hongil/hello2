38fea73423720cac7b6841ba8e891711b37319aa
hongil <shi8295@naver.com>
2020-05-27 17:29:12 +0900

    Step02_DataType 프로젝트 추가함

---
+++ Step02_DataType/src/test/main/MainClass06.java
@@ -0,0 +1,18 @@
+package test.main;
+
+public class MainClass06 {
+	public static void main(String[] args) {
+		// int type 지역변수를 선언만 하면 만들어 지지 않는다.
+		int num1;
+		// String type 지역변수를 선언만 하면 만들어 지지 않는다.
+		String name1;
+		
+		//아직 만들어 지지 않았기 때문에 num1 은 참조 불가
+		//int result=10+num1;
+		
+		//아직 만들어 지지 않았기 때문에 name1 은 참조 불가
+		//System.out.println("name1:"+name1);
+		
+		System.out.println("main 이벤트가 종료 됩니다.");
+	}
+}
