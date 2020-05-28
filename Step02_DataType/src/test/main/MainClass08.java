bb3596154cb88841fedf8acd6270af93cd66cd16
hongil <shi8295@naver.com>
2020-05-28 09:54:56 +0900

    MainClass08 수정함

--- Step02_DataType/src/test/main/MainClass08.java
+++ Step02_DataType/src/test/main/MainClass08.java
@@ -3,6 +3,7 @@ package test.main;
 public class MainClass08 {
 	public static void main(String[] args) {
 		System.out.println("Git test");
-		System.out.println("");
+		System.out.println("변경1");
+		System.out.println("변경2");
 	}
 }
